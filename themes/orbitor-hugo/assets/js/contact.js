a134941a98fa68a6e3d51d3e885c5a3886053022
jjurm <micko.juraj64@gmail.com>
2020-08-07 09:44:40 +0200

    Improve details in contact section

--- themes/orbitor-hugo/assets/js/contact.js
+++ themes/orbitor-hugo/assets/js/contact.js
@@ -11,7 +11,7 @@
         message.text(response);
         setTimeout(function () {
             message.fadeOut();
-        }, 2000);
+        }, 5000);
         form.find('input:not([type="submit"]), textarea').val('');
     }
 
@@ -21,7 +21,7 @@
         message.text(data.responseText);
         setTimeout(function () {
             message.fadeOut();
-        }, 2000);
+        }, 5000);
     }
     
     form.submit(function (e) {
