c9375440fcb03bfadb5a492ed3cf2ea8ce94c846
jjurm <micko.juraj64@gmail.com>
2020-08-15 12:56:49 +0200

    Use SMTP for sending emails

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,37 @@
 const functions = require("firebase-functions");
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 const cors = require("cors")({ origin: true });
 
-sgMail.setApiKey(functions.config().contact.sendgrid.api.key);
+let transporter = nodemailer.createTransport({
+  host: functions.config().smtp.host,
+  port: parseInt(functions.config().smtp.port),
+  secure: true,
+  auth: {
+    user: functions.config().smtp.user,
+    pass: functions.config().smtp.pass,
+  },
+});
 
 exports.sendMail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     const email = req.body.email;
     const subject = req.body.subject;
-    const message = req.body.message;
-    console.log('Received an email from %s, with subject %s, saying: %s', email, subject, message);
+    const message = `${req.body.message}\n\n--\nThis e-mail was sent from a contact form on Hack Kosice (https://hackkosice.com)`;
+    console.log('Received a message from %s, with subject %s, saying: %s', email, subject, message);
     const msg = {
-        to: 'contact@hackkosice.com',
-        from: 'contact@hackkosice.com',
-        reply_to: email,
-        subject: subject,
-        text: message,
+      from: 'Hack Kosice <contact@hackkosice.com>',
+      to: 'Hack Kosice <contact@hackkosice.com>',
+      reply_to: email,
+      subject: subject,
+      text: message,
     }
 
     // returning result
-    return sgMail.send(msg, (error, result) => {
-        if (error) {
-            return res.status(500).send(error.toString());
-        }
-        return res.send("Message Sent");
+    return transporter.sendMail(msg, (error, result) => {
+      if (error) {
+        return res.status(500).send(error.toString());
+      }
+      return res.send("Message Sent");
     });
   });
 });
