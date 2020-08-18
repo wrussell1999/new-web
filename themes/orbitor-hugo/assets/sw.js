872ab91df30ed869332ae21224411fd25b151f35
jjurm <micko.juraj64@gmail.com>
2020-08-18 21:56:50 +0200

    Register service worker script

---
+++ themes/orbitor-hugo/assets/sw.js
@@ -0,0 +1,41 @@
+var cacheName = 'hackkosice-v1';
+var contentToCache = ["/"];
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(contentToCache);
+        })
+    );
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            if (event.request.method === 'GET') {
+                let response2 = response.clone();
+                caches.open(cacheName).then(function (cache) {
+                    cache.put(event.request, response2);
+                });
+            }
+            return response;
+        }).catch(function() {
+            console.log('[Service Worker] Got fetch request: ' + event.request.url);
+            return caches.open(cacheName).then(function (cache) {
+                return cache.match(event.request).then(function (response) {
+                    return response;
+                });
+            });
+        })
+    );
+});
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
